bfc8d0f40894d0037436d63da81401a85adad400
Mohamed Samir <mohamed.samir@yoomah.com>
2022-08-23 15:51:05 +0200

    Adding Search filter

--- cms/djangoapps/contentstore/courseware_index.py
+++ cms/djangoapps/contentstore/courseware_index.py
@@ -541,10 +541,18 @@ class AboutInfo:
 
         return [mode.slug for mode in CourseMode.modes_for_course(course.id)]
 
+    # Added by Mohamed Samir
+    def from_other_settings(self, **kwargs):
+        course = kwargs.get('course', None)
+        if not course:
+            raise ValueError("Context dictionary does not contain expected argument 'course' ")
+        return course.other_course_settings.get('subject')
+
     # Source location options - either from the course or the about info
     FROM_ABOUT_INFO = from_about_dictionary
     FROM_COURSE_PROPERTY = from_course_property
     FROM_COURSE_MODE = from_course_mode
+    FROM_OTHER_SETTINGS = from_other_settings
 
 
 class CourseAboutSearchIndexer(CoursewareSearchIndexer):
@@ -589,6 +597,7 @@ class CourseAboutSearchIndexer(CoursewareSearchIndexer):
         AboutInfo("language", AboutInfo.PROPERTY, AboutInfo.FROM_COURSE_PROPERTY),
         AboutInfo("invitation_only", AboutInfo.PROPERTY, AboutInfo.FROM_COURSE_PROPERTY),
         AboutInfo("catalog_visibility", AboutInfo.PROPERTY, AboutInfo.FROM_COURSE_PROPERTY),
+        AboutInfo("subject", AboutInfo.PROPERTY, AboutInfo.FROM_OTHER_SETTINGS),
     ]
 
     @classmethod
